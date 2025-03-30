c908ff4b901a8634405300d2efaaba06fe4c6b56
Antonio Queiroz <antonioqueiroz.dev@gmail.com>
2025-03-30 16:10:39 -0300

    feat: adicionar menu principal

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,27 @@
 package main;
 
 import repository.Filerepository;
-
-import java.io.File;
+import services.ImprimirMenu;
 
 public class Main {
     public static void main(String[] args) {
         Filerepository file = new Filerepository();
         file.criarFile();
         file.lerFile();
+        ImprimirMenu imprimirMenu = new ImprimirMenu();
+        int opcao = 0;
+
+        while (opcao != 6) {
+            opcao = imprimirMenu.imprimirMenuPrincipal();
+            switch (opcao) {
+                case 1:
+                    break;
+                case 6:
+                    System.out.println("Encerrando...");
+                    break;
+                default:
+                    System.out.println("Opção inválida, tente novamente!");
+            }
+        }
     }
 }
